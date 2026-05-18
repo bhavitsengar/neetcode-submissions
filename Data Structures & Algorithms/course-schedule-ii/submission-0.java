bf48abacc572085c01bd3eabfa586ae3370b3ae1
Bhavit Singh Sengar <bhavit.s.sengar@gmail.com>
2026-05-18 20:08:31 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
+        int[] indegree = new int[numCourses];
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for(int[] edge : prerequisites) {
+            indegree[edge[0]]++;
+
+            List<Integer> neighbours = graph.getOrDefault(edge[1], new ArrayList<>());
+
+            neighbours.add(edge[0]);
+
+            graph.put(edge[1], neighbours);
+
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i<numCourses; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int[] visited = new int[numCourses];
+
+        while(!queue.isEmpty()) {
+
+            int course = queue.poll();
+            
+            result.add(course);
+
+            List<Integer> neighbours = graph.getOrDefault(course, new ArrayList<>());
+
+            for(int nextCourse : neighbours) {
+
+                if(--indegree[nextCourse] == 0) {
+                    queue.add(nextCourse);
+                }
+            }
+
+        }
+
+        if(result.size() == numCourses)
+            return result.stream()
+                           .mapToInt(i -> i)
+                           .toArray();
+
+        return new int[]{};
+
+    }
+}
